244bce3fdfd1a96905a940126cf650c4aa76ad2d
chenshuxian <j1092333120@gmail.com>
2022-03-06 13:07:47 +0800

    pm2 config git hooks

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // server.js
-let FileStreamRotator = require("file-stream-rotator");
-const express = require("express");
-const next = require("next");
-const fs = require("fs");
-const url = require("url");
-var morgan = require("morgan");
-var path = require("path");
+let FileStreamRotator = require('file-stream-rotator');
+const express = require('express');
+const next = require('next');
+const fs = require('fs');
+const url = require('url');
+var morgan = require('morgan');
+var path = require('path');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const http = require("http");
-const https = require("https");
+const http = require('http');
+const https = require('https');
 
-const dayjs = require("dayjs");
-const exp = require("constants");
+const dayjs = require('dayjs');
+const exp = require('constants');
 
 // var options = {
 //   key: fs.readFileSync("./ssl/old/lieyu.fantasyball.tw.key"),
@@ -24,34 +24,33 @@ const exp = require("constants");
 // };
 
 app.prepare().then(() => {
-  const app = express();
+	const app = express();
 
-  var logDirectory = path.join(__dirname, "log");
+	// var logDirectory = path.join(__dirname, 'log');
 
-  // ensure log directory exists
-  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+	// // ensure log directory exists
+	// fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-  // create a rotating write stream
-  var accessLogStream = FileStreamRotator.getStream({
-    date_format: "YYYYMMDD",
-    filename: path.join(logDirectory, "access-%DATE%.log"),
-    frequency: "daily",
-    verbose: false,
-  });
-   // setup the logger
-   app.use(morgan("combined", { stream: accessLogStream }));
-  // create static server
-  app.use(express.static(path.join(__dirname + '/static/download')));
- 
+	// // create a rotating write stream
+	// var accessLogStream = FileStreamRotator.getStream({
+	// 	date_format: 'YYYYMMDD',
+	// 	filename: path.join(logDirectory, 'access-%DATE%.log'),
+	// 	frequency: 'daily',
+	// 	verbose: false,
+	// });
+	// // setup the logger
+	// app.use(morgan('combined', { stream: accessLogStream }));
+	// create static server
+	app.use(express.static(path.join(__dirname + '/static/download')));
 
-  app.all("*", async (req, res) => {
-    return handle(req, res);
-  });
+	app.all('*', async (req, res) => {
+		return handle(req, res);
+	});
 
-  var httpsServer = http.createServer(app);
+	var httpsServer = http.createServer(app);
 
-  httpsServer.listen(3000, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${3000}`);
-  });
+	httpsServer.listen(3000, (err) => {
+		if (err) throw err;
+		console.log(`> Ready on http://localhost:${3000}`);
+	});
 });
