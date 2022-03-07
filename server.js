2fcdb07196a23619beb354315ca4ac6297f61a5b
chenshuxian <j1092333120@gmail.com>
2022-03-07 08:52:03 +0800

    fix server bug not module

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // server.js
-let FileStreamRotator = require('file-stream-rotator');
+// let FileStreamRotator = require('file-stream-rotator');
 const express = require('express');
 const next = require('next');
 const fs = require('fs');
 const url = require('url');
-var morgan = require('morgan');
+// var morgan = require('morgan');
 var path = require('path');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
