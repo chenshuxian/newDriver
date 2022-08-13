966bdc84351a17371e4bd117798faac2f92c6fd0
chenshuxian <j1092333120@gmail.com>
2022-08-13 12:15:14 +0800

    fix index chartjs bug

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -3,11 +3,11 @@ import { Box, Container, Grid } from '@mui/material';
 import { Budget } from '../../components/dashboard/budget';
 import { LatestOrders } from '../../components/dashboard/latest-orders';
 import { LatestProducts } from '../../components/dashboard/latest-products';
-import { Sales } from '../../components/dashboard/sales';
+//import { Sales } from '../../components/dashboard/sales';
 import { TasksProgress } from '../../components/dashboard/tasks-progress';
 import { TotalCustomers } from '../../components/dashboard/total-customers';
 import { TotalProfit } from '../../components/dashboard/total-profit';
-import { TrafficByDevice } from '../../components/dashboard/traffic-by-device';
+// import { TrafficByDevice } from '../../components/dashboard/traffic-by-device';
 import { DashboardLayout } from '../../components/dashboard-layout';
 
 const Dashboard = () => (
@@ -32,12 +32,12 @@ const Dashboard = () => (
 					<Grid item xl={3} lg={3} sm={6} xs={12}>
 						<TasksProgress />
 					</Grid>
-					<Grid item lg={8} md={12} xl={9} xs={12}>
+					{/* <Grid item lg={8} md={12} xl={9} xs={12}>
 						<Sales />
-					</Grid>
-					<Grid item lg={4} md={6} xl={3} xs={12}>
+					</Grid> */}
+					{/* <Grid item lg={4} md={6} xl={3} xs={12}>
 						<TrafficByDevice sx={{ height: '100%' }} />
-					</Grid>
+					</Grid> */}
 					<Grid item lg={4} md={6} xl={3} xs={12}>
 						<LatestProducts sx={{ height: '100%' }} />
 					</Grid>
